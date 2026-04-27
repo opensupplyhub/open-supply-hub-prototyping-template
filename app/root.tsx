56fc4751102bc1d2ab808df8995a85391f8d9fd2
Stephan Protsack <stephan@speedandfunction.com>
2026-04-27 18:49:43 +0200

    Add favicon support and update root component

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,30 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/favicon/apple-touch-icon.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon/favicon-16x16.png",
+  },
+  { rel: "icon", href: "/favicon/favicon.ico", sizes: "any" },
+  { rel: "manifest", href: "/favicon/site.webmanifest" },
+  {
+    rel: "mask-icon",
+    href: "/favicon/safari-pinned-tab.svg",
+    color: "#000000",
+  },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
